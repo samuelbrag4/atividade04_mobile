469474534beded306643d3e0408bfe1d4c16b24e
samuelbrag4 <samuelbraga.tds2.senai@gmail.com>
2025-04-03 09:33:41 -0300

    feat: add background image and style home screen

--- app/index.jsx
+++ app/index.jsx
@@ -1,23 +1,33 @@
 import { View, Text, StyleSheet, ImageBackground } from "react-native";
-import Link  from "expo-router";
+import Link from "expo-router";
 
 export default function Screen() {
     return (
-        <ImageBackground>
-
+        <ImageBackground
+            source={require("../assets/image 9.png")} // Caminho da imagem de fundo
+            style={styles.background}
+        >
+            <View style={styles.container}>
+                <Text style={styles.title}>Olá, Tudo bem?</Text>
+                {/* Adicione mais elementos aqui */}
+            </View>
         </ImageBackground>
     );
 }
 
 const styles = StyleSheet.create({
+    background: {
+        flex: 1,
+        resizeMode: "cover",
+    },
     container: {
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
-        backgroundColor: '#FFFFFF',
     },
     title: {
         fontSize: 24,
-        fontWeight: 'bold'
-    }
+        fontWeight: 'bold',
+        color: '#fff',
+    },
 });
